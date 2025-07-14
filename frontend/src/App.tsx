c79501b6e20829d034ed14a56049d8f44f3d7fe9
Guilherme Gouveia Campos <guilherme.g.campos@academico.unirv.edu.br>
2025-07-13 21:50:32 -0300

    Etapa 1 - Projeto completo iniciado com estrutura do frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind funcionando!</h1>
+    </div>
+  );
+}
+
+export default App;
